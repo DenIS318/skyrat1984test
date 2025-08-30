ba50f505171335b4e95638e05acd1425b8d669a8
denis318 <11572080+DenIS318@users.noreply.github.com>
2025-08-30 14:39:15 +0500

    fix: cargo ui plasmacost for express & pda

--- tgui/packages/tgui/interfaces/CargoExpress.tsx
+++ tgui/packages/tgui/interfaces/CargoExpress.tsx
@@ -26,6 +26,7 @@ type Data = {
   canBeacon: BooleanLike;
   printMsg: string;
   message: string;
+  current_plasma_cost: number | string; // SS1984 ADDITION
 };
 
 export function CargoExpress(props) {
@@ -70,6 +71,7 @@ function CargoExpressContent(props) {
     beaconName,
     canBuyBeacon,
     printMsg,
+    current_plasma_cost, // SS1984 ADDITION
   } = data;
 
   return (
@@ -79,8 +81,21 @@ function CargoExpressContent(props) {
           title="Cargo Express"
           buttons={
             <Box inline bold verticalAlign={'middle'}>
+              {/* SS1984 REMOVAL START
               <AnimatedNumber value={Math.round(points)} />
               {' credits'}
+              SS1984 REMOVAL END */}
+              {/* SS1984 ADDITION START */}
+                <>
+                  <Box inline bold verticalAlign="middle" style={{ marginRight: '1em' }}>
+                    <span style={{ color: '#BA3692' }}>Plasma</span> cost: {current_plasma_cost}
+                  </Box>
+                  <Box inline bold verticalAlign="middle">
+                    <AnimatedNumber value={Math.round(points)} />
+                    {' credits'}
+                  </Box>
+                </>
+              {/* SS1984 ADDITION END */}
             </Box>
           }
         >
